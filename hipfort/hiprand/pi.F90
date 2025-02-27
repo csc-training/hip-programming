f19715691500ebd966bf0844fdda3bc4eda338d8
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2025-02-27 21:07:19 +0200

    Update pi.F90

--- hipfort/hiprand/pi.F90
+++ hipfort/hiprand/pi.F90
@@ -1,9 +1,13 @@
 program rand_test
   use iso_c_binding
   use iso_fortran_env, only : INT64
+  ! TODO Add here the necessary modules for the GPU operations
   use hipfort
   use hipfort_check 
   use hipfort_hiprand
+
+  !OPTIONAL
+  !TODO write an interfac to the C wrapper which calls the reduction kernel.
 
   implicit none
 
@@ -68,21 +72,32 @@ contains
     allocate(x(1:n))
     allocate(y(1:n))
     Nbytes=sizeof(x)
-
-  call hipCheck(hipMalloc(x_d,Nbytes))
-  call hipCheck(hipMalloc(y_d,Nbytes))
+    
+    call hipCheck(hipMalloc(x_d,Nbytes))
+    call hipCheck(hipMalloc(y_d,Nbytes))
 
     inside = 0
+    ! Initialization for (optiional) task
+    ! call hipCheck(hipMalloc(inside_d,Sbytes)) ! Sbytes = sizeof(inside)
+    ! call hipCheck(hipMemcpy( inside_d_d, Sbytes, hipMemcpyHostToDevice))
 
-
+    ! TODO  Initialize the gpu random number generator 
     istat= hiprandCreateGenerator(gen, HIPRAND_RNG_PSEUDO_DEFAULT)
 
+    ! TODO  Fill the arrays x and y with random uniform distributed numbers 
     istat= hiprandGenerateUniform(gen, x_d, n)
     istat= hiprandGenerateUniform(gen, y_d, n)
+    
+    ! TODO copy the random numbers from GPU to CPU 
+    call hipCheck(hipMemcpy(c_loc(x), x_d, Nbytes, hipMemcpyDeviceToHost))
+    call hipCheck(hipMemcpy(c_loc(y), y_d, Nbytes, hipMemcpyDeviceToHost))
 
-  call hipCheck(hipMemcpy(c_loc(x), x_d, Nbytes, hipMemcpyDeviceToHost))
-  call hipCheck(hipMemcpy(c_loc(y), y_d, Nbytes, hipMemcpyDeviceToHost))
-
+    ! TODO Bonus exercise: replace the below reduction loop  done on the CPU with a GPU kernel
+    ! The kernel is in the hip_kernels.cpp file.
+    ! You need to implement an interface to call the C function simialrly to the saxpy example
+    ! Note that in this case there is no need to transfer the x and y arrays to CPU, 
+    ! You only need to copy the final result. For this you can define an extra array on GPU of lenth 1.
+    ! 
     do i = 1, n
       if (x(i)**2 + y(i)**2 < 1.0) then
         inside = inside + 1
