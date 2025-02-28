a65c820f6354776202137211081abc9453e50a5c
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2025-02-28 15:19:11 +0200

    Create hip_kernels.cpp

---
+++ hipfort/hiprand/solution_bonus/hip_kernels.cpp
@@ -0,0 +1,26 @@
+#include <hip/hip_runtime.h>
+#include <cstdio>
+
+__global__ void countInsideKernel(float *x, float *y, int *inside, long long int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (idx < n) {
+        if (x[idx] * x[idx] + y[idx] * y[idx] < 1.0f) {
+            // Atomic increment to avoid race condition
+            atomicAdd(inside, 1);
+        }
+    }
+}
+
+extern "C"
+{
+  void launch(float *x, float *y, int *inside_d, long long int N)
+  {
+
+     dim3 tBlock(256,1,1);
+     dim3 grid(ceil((float)N/tBlock.x),1,1);
+    
+     countInsideKernel<<<grid, tBlock>>>( x, y, inside_d, N);
+  }
+}
